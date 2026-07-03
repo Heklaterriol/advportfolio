b112c3006f96c34cda1784ebe6e12a7d24e98082
Hekla Terriol <webstuff@sternenaugen.net>
2026-07-03 22:02:29 +0200

    Update install.php with Joomla 6 Table API

--- install.php
+++ install.php
@@ -7,13 +7,17 @@
 // No direct access.
 defined('_JEXEC') or die;
 
+use JoomlaCMSTableTable;
+use JoomlaCMSFactory;
+
 /**
  * Advanced Portfolio Installer Script
  *
  * @package		Joomla.Install
- * @subpakage	Skyline.AdvPortfolio
+ * @subpakage	AdvPortfolio
  */
-class Com_AdvPortfolioInstallerScript {
+class Com_AdvPortfolioInstallerScript
+{
 
 	/**
 	 * Install.
@@ -21,21 +25,20 @@ class Com_AdvPortfolioInstallerScript {
 	 * @param	$parent
 	 */
 	public function install($parent) {
-		/** @var JTableContentType $table */
-		$table	= JTable::getInstance('contenttype');
+		$table = Table::getInstance('contenttype');
 
 		if ($table) {
-			$table->load(array('type_alias' => 'com_advportfolio.project'));
+			$table->load(['type_alias' => 'com_advportfolio.project']);
 
 			if (!$table->type_id) {
-				$data	= array(
-					'type_title'		=> 'Portfolio Project',
-					'type_alias'		=> 'com_advportfolio.project',
-					'table'				=> '{"special":{"dbtable":"#__advportfolio_projects","key":"id","type":"Project","prefix":"ProjectsTable","config":"array()"},"common":{"dbtable":"#__core_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
-					'rules'				=> '',
-					'field_mappings'	=> '{"common":[{"core_content_item_id":"id","core_title":"title","core_state":"state","core_alias":"alias","core_created_time":"created","core_modified_time":"modified","core_body":"description", "core_hits":"null","core_publish_up":"null","core_publish_down":"null","core_access":"access", "core_params":"attribs", "core_featured":"null", "core_metadata":"metadata", "core_language":"language", "core_images":"images", "core_urls":"link", "core_version":"null", "core_ordering":"ordering", "core_metakey":"metakey", "core_metadesc":"metadesc", "core_catid":"catid", "core_xreference":"null", "asset_id":"null"}], "special": []}',
-					'router'			=> 'AdvPortfolioHelperRoute::getProjectRoute',
-				);
+				$data = [
+					'type_title' => 'Portfolio Project',
+					'type_alias' => 'com_advportfolio.project',
+					'table' => '{"special":{"dbtable":"#__advportfolio_projects","key":"id","type":"Project","prefix":"ProjectsTable","config":"array()"},"common":{"dbtable":"#__core_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
+					'rules' => '',
+					'field_mappings' => '{"common":[{"core_content_item_id":"id","core_title":"title","core_state":"state","core_alias":"alias","core_created_time":"created","core_modified_time":"modified","core_body":"description", "core_hits":"null","core_publish_up":"null","core_publish_down":"null","core_access":"access", "core_params":"attribs", "core_featured":"null", "core_metadata":"metadata", "core_language":"language", "core_images":"images", "core_urls":"link", "core_version":"null", "core_ordering":"ordering", "core_metakey":"metakey", "core_metadesc":"metadesc", "core_catid":"catid", "core_xreference":"null", "asset_id":"null"}], "special": []}',
+					'router' => 'AdvPortfolioHelperRoute::getProjectRoute',
+				];
 
 				$table->bind($data);
 
