d3940ccdfd1979bffb5a6ff2a4c94e09313fc681
Hekla Terriol <webstuff@sternenaugen.net>
2026-07-03 21:19:02 +0200

    Update site view templates for Joomla 6 with WebAssetManager

--- site/views/project/tmpl/default.php
+++ site/views/project/tmpl/default.php
@@ -7,96 +7,66 @@
 // No direct access.
 defined('_JEXEC') or die;
 
-JHtml::_('jquery.framework');
-JHtml::_('stylesheet', 'com_advportfolio/style.css', false, true);
+use JoomlaCMSFactory;
+use JoomlaCMSLanguageText;
+use JoomlaCMSHelperStringHelper;
 
-$hasMedia	= ($this->item->type == 0 && count($this->item->images)) || ($this->item->type == 1 && $this->item->video_link);
-$hasContent	= $this->item->description != '' && $this->params->def('show_description', 1);
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->useScript('jquery');
+$wa->useStyle('com_advportfolio.style');
+
+$hasMedia = ($this->item->type == 0 && count($this->item->images)) || ($this->item->type == 1 && $this->item->video_link);
+$hasContent = $this->item->description != '' && $this->params->get('show_description', 1);
 $this->params->def('description_position', 'right');
 $this->params->def('description_width', '4');
 ?>
-
 <div class="item-page<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->params->get('show_page_heading')) : ?>
-		<div class="page-header">
-			<h1>
-				<?php echo $this->escape($this->params->get('page_heading')); ?>
-			</h1>
-
-			<?php if (!$this->params->get('show_title')) : ?>
-				<div class="project-nav">
-					<a class="prev-project<?php echo $this->prevItem ? '' : ' disable'; ?>" href="<?php echo $this->prevItem ? AdvPortfolioHelperRoute::getProjectRoute($this->prevItem->slug) : 'javascript:void(0);'; ?>"></a>
-					<a class="next-project<?php echo $this->nextItem ? '' : ' disable'; ?>" href="<?php echo $this->nextItem ? AdvPortfolioHelperRoute::getProjectRoute($this->nextItem->slug) : 'javascript:void(0);'; ?>"></a>
-				</div>
-			<?php endif; ?>
-		</div>
-	<?php endif; ?>
-
-	<?php if ($this->params->get('show_title', true)) : ?>
+<?php if ($this->params->get('show_page_heading')) : ?>
 	<div class="page-header">
-		<h2 itemprop="name">
-			<?php echo $this->escape($this->item->title); ?>
-		</h2>
-
+		<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
+		<?php if (!$this->params->get('show_title')) : ?>
 		<div class="project-nav">
 			<a class="prev-project<?php echo $this->prevItem ? '' : ' disable'; ?>" href="<?php echo $this->prevItem ? AdvPortfolioHelperRoute::getProjectRoute($this->prevItem->slug) : 'javascript:void(0);'; ?>"></a>
 			<a class="next-project<?php echo $this->nextItem ? '' : ' disable'; ?>" href="<?php echo $this->nextItem ? AdvPortfolioHelperRoute::getProjectRoute($this->nextItem->slug) : 'javascript:void(0);'; ?>"></a>
 		</div>
-	</div>
-	<?php endif; ?>
-
-	<div id="project-wrapper" class="project-wrapper clearfix">
-		<?php if ($hasMedia && $hasContent) : ?>
-
-			<?php if ($this->params->get('description_position') == 'top' || $this->params->get('description_position') == 'bottom') : ?>
-				<?php if ($this->params->get('description_position') == 'top') : ?>
-					<div>
-						<?php echo $this->loadTemplate('description'); ?>
-					</div>
-				<?php endif; ?>
-
-				<div>
-					<?php echo $this->loadTemplate('media'); ?>
-				</div>
-
-				<?php if ($this->params->get('description_position') == 'bottom') : ?>
-					<div>
-						<?php echo $this->loadTemplate('description'); ?>
-					</div>
-				<?php endif; ?>
-
-			<?php else : ?>
-
-				<?php if ($this->params->get('description_position') == 'left') : ?>
-					<div class="span<?php echo $this->params->get('description_width'); ?>">
-						<?php echo $this->loadTemplate('description'); ?>
-					</div>
-				<?php endif; ?>
-
-				<div class="span<?php echo (12 - $this->params->get('description_width')); ?>">
-					<?php echo $this->loadTemplate('media'); ?>
-				</div>
-
-				<?php if ($this->params->get('description_position') == 'right') : ?>
-					<div class="span<?php echo $this->params->get('description_width'); ?>">
-						<?php echo $this->loadTemplate('description'); ?>
-					</div>
-				<?php endif; ?>
-
-			<?php endif; ?>
-
-		<?php else : ?>
-
-			<?php if ($hasMedia) : ?>
-				<div>
-					<?php echo $this->loadTemplate('media'); ?>
-				</div>
-			<?php else : ?>
-				<?php echo $this->loadTemplate('description'); ?>
-			<?php endif; ?>
-
 		<?php endif; ?>
 	</div>
+<?php endif; ?>
+<?php if ($this->params->get('show_title', true)) : ?>
+<div class="page-header">
+	<h2 itemprop="name"><?php echo $this->escape($this->item->title); ?></h2>
+	<div class="project-nav">
+		<a class="prev-project<?php echo $this->prevItem ? '' : ' disable'; ?>" href="<?php echo $this->prevItem ? AdvPortfolioHelperRoute::getProjectRoute($this->prevItem->slug) : 'javascript:void(0);'; ?>"></a>
+		<a class="next-project<?php echo $this->nextItem ? '' : ' disable'; ?>" href="<?php echo $this->nextItem ? AdvPortfolioHelperRoute::getProjectRoute($this->nextItem->slug) : 'javascript:void(0);'; ?>"></a>
+	</div>
 </div>
-
+<?php endif; ?>
+<div id="project-wrapper" class="project-wrapper clearfix">
+<?php if ($hasMedia && $hasContent) : ?>
+	<?php if ($this->params->get('description_position') == 'top' || $this->params->get('description_position') == 'bottom') : ?>
+		<?php if ($this->params->get('description_position') == 'top') : ?>
+			<div><?php echo $this->loadTemplate('description'); ?></div>
+		<?php endif; ?>
+		<div><?php echo $this->loadTemplate('media'); ?></div>
+		<?php if ($this->params->get('description_position') == 'bottom') : ?>
+			<div><?php echo $this->loadTemplate('description'); ?></div>
+		<?php endif; ?>
+	<?php else : ?>
+		<?php if ($this->params->get('description_position') == 'left') : ?>
+			<div class="span<?php echo $this->params->get('description_width'); ?>"><?php echo $this->loadTemplate('description'); ?></div>
+		<?php endif; ?>
+		<div class="span<?php echo (12 - $this->params->get('description_width')); ?>"><?php echo $this->loadTemplate('media'); ?></div>
+		<?php if ($this->params->get('description_position') == 'right') : ?>
+			<div class="span<?php echo $this->params->get('description_width'); ?>"><?php echo $this->loadTemplate('description'); ?></div>
+		<?php endif; ?>
+	<?php endif; ?>
+<?php else : ?>
+	<?php if ($hasMedia) : ?>
+		<div><?php echo $this->loadTemplate('media'); ?></div>
+	<?php else : ?>
+		<?php echo $this->loadTemplate('description'); ?>
+	<?php endif; ?>
+<?php endif; ?>
+</div>
+</div>
 <?php echo AdvPortfolioHelper::poweredBy();
--- site/views/projects/tmpl/default.php
+++ site/views/projects/tmpl/default.php
@@ -7,117 +7,99 @@
 // No direct access.
 defined('_JEXEC') or die;
 
+use JoomlaCMSFactory;
+use JoomlaCMSLanguageText;
+use JoomlaCMSHelperStringHelper;
 
-// Include CSS and JS
-JHtml::_('jquery.framework');
-JHtml::_('script', 'com_advportfolio/jquery.isotope.js', false, true);
-JHtml::_('stylesheet', 'com_advportfolio/style.css', false, true);
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->useScript('jquery');
+$wa->useScript('com_advportfolio.isotope');
+$wa->useStyle('com_advportfolio.style');
 
-$columns		= $this->params->def('num_columns', 3);
-$item_class		= ' column-' . $columns;
-$image_width	= 1200 / $columns;
+$columns = $this->params->get('num_columns', 3);
+$item_class = ' column-' . $columns;
+$image_width = 1200 / $columns;
 
-$this->document->addScriptDeclaration('
+$wa->addInlineScript("
 (function($) {
-	$(window).load(function() {
-		var $container = $("#projects-wrapper");
-
-		$container.isotope({
-			itemSelector:".isotope-item"
-		});
-
-		var $optionSets = $("#projects-filter .option-set"),
-			$optionLinks = $optionSets.find("a");
-
-		$optionLinks.click(function () {
-			var $this = $(this);
-			$this.stop();
-			// don"t proceed if already selected
-			if ($this.hasClass("selected")) {
-				return false;
-			}
-			var $optionSet = $this.parents(".option-set");
-			$optionSet.find(".selected").removeClass("selected");
-			$this.addClass("selected");
-
-			// make option object dynamically, i.e. { filter: ".my-filter-class" }
-			var options = {},
-					key = $optionSet.attr("data-option-key"),
-					value = $this.attr("data-option-value");
-			// parse "false" as false boolean
-			value = value === "false" ? false : value;
-			options[ key ] = value;
-
-			// otherwise, apply new options
-			$container.isotope(options);
-
-			return false;
-		});
-	});
-})(jQuery);
-');
+$(document).ready(function() {
+var $container = $("#projects-wrapper");
+if (typeof $.fn.masonry !== 'undefined') {
+$container.masonry({itemSelector: '.isotope-item', columnWidth: '.isotope-item', percentPosition: true});
+} else if (typeof $.fn.isotope !== 'undefined') {
+$container.isotope({itemSelector: '.isotope-item'});
+}
+var $optionSets = $("#projects-filter .option-set"), $optionLinks = $optionSets.find("a");
+$optionLinks.click(function () {
+var $this = $(this);
+$this.stop();
+if ($this.hasClass("selected")) { return false; }
+var $optionSet = $this.parents(".option-set");
+$optionSet.find(".selected").removeClass("selected");
+$this.addClass("selected");
+var options = {}, key = $optionSet.attr("data-option-key"), value = $this.attr("data-option-value");
+value = value === "false" ? false : value;
+options[ key ] = value;
+if (typeof $.fn.isotope !== 'undefined') {
+$container.isotope(options);
+} else if (typeof $.fn.masonry !== 'undefined') {
+$container.masonry('layout');
+}
+return false;
+});
+});
+}(jQuery));
+", []);
 ?>
-
 <div class="portfolio-list<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->params->get('show_page_heading', 1)) : ?>
-		<div class="page-header">
-			<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
-		</div>
-	<?php endif; ?>
-
-	<div class="projects-filter" id="projects-filter">
-		<ul class="option-set" data-option-key="filter">
-			<li>
-				<a href="#filter" class="selected" data-option-value="*">
-					<?php echo JText::_('COM_ADVPORTFOLIO_FILTER_ALL'); ?>
-				</a>
-			</li>
-			<?php foreach ($this->tags as $tag) : ?>
-			<li>
-				<a href="#filter" data-option-value=".<?php echo JApplication::stringURLSafe($tag); ?>"><?php echo $tag; ?></a>
-			</li>
-			<?php endforeach; ?>
-		</ul>
+<?php if ($this->params->get('show_page_heading', 1)) : ?>
+	<div class="page-header">
+		<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 	</div>
-
-	<div class="clearfix projects-wrapper" id="projects-wrapper">
-		<?php foreach ($this->items as $item) :
-			$link	= AdvPortfolioHelperRoute::getProjectRoute($item->slug);
-			$class	= '';
-			foreach ($item->tags as $tag) {
-				$class .= ' ' . JApplication::stringURLSafe($tag);
-			}
-		?>
-
-		<div class="isotope-item project-<?php echo $item->id . $class . $item_class; ?>">
-			<div class="project-img">
-				<a href="<?php echo $link; ?>">
-					<?php echo JHtml::_('advportfolio.image', $item->thumbnail, $image_width, null, $this->escape($item->title)); ?>
-					<div class="img-overlay"></div>
-				</a>
-			</div>
-			<div class="project-item-meta">
-				<h4>
-					<a rel="bookmark" title="<?php echo JText::_('COM_ADVPORTFOLIO_PERMALINK_TO') . ' ' . $item->title; ?>" href="<?php echo $link; ?>">
-						<?php echo $item->title; ?>
-					</a>
-				</h4>
-				<?php if ($this->params->get('show_short_description', 1)) : ?>
-					<?php echo $item->short_description; ?>
-				<?php endif; ?>
-			</div>
-		</div>
-
+<?php endif; ?>
+<div class="projects-filter" id="projects-filter">
+	<ul class="option-set" data-option-key="filter">
+		<li><a href="#filter" class="selected" data-option-value="*"><?php echo Text::_('COM_ADVPORTFOLIO_FILTER_ALL'); ?></a></li>
+		<?php foreach ($this->tags as $tag) : ?>
+		<li><a href="#filter" data-option-value=".<?php echo StringHelper::stringURLSafe($tag); ?>"><?php echo $tag; ?></a></li>
 		<?php endforeach; ?>
-	</div>
-
-	<?php if (($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
-	<div class="pagination">
-		<?php  if ($this->params->def('show_pagination_results', 1)) : ?>
-		<p class="counter pull-right"><?php echo $this->pagination->getPagesCounter(); ?></p>
-		<?php endif; ?>
-		<?php echo $this->pagination->getPagesLinks(); ?> </div>
-	<?php  endif; ?>
+	</ul>
 </div>
-
+<div class="clearfix projects-wrapper" id="projects-wrapper">
+<?php foreach ($this->items as $item) :
+	$link = AdvPortfolioHelperRoute::getProjectRoute($item->slug);
+	$class = '';
+	foreach ($item->tags as $tag) {
+		$class .= ' ' . StringHelper::stringURLSafe($tag);
+	}
+?>
+<div class="isotope-item project-<?php echo $item->id . $class . $item_class; ?>">
+	<div class="project-img">
+		<a href="<?php echo $link; ?>">
+			<?php echo AdvPortfolioHelper::renderImage($item->thumbnail, $image_width, null, $this->escape($item->title)); ?>
+			<div class="img-overlay"></div>
+		</a>
+	</div>
+	<div class="project-item-meta">
+		<h4>
+			<a rel="bookmark" title="<?php echo Text::_('COM_ADVPORTFOLIO_PERMALINK_TO') . ' ' . $item->title; ?>" href="<?php echo $link; ?>">
+				<?php echo $item->title; ?>
+			</a>
+		</h4>
+		<?php if ($this->params->get('show_short_description', 1)) : ?>
+			<?php echo $item->short_description; ?>
+		<?php endif; ?>
+	</div>
+</div>
+<?php endforeach; ?>
+</div>
+<?php if (($this->params->get('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
+<div class="pagination">
+	<?php if ($this->params->get('show_pagination_results', 1)) : ?>
+	<p class="counter pull-right"><?php echo $this->pagination->getPagesCounter(); ?></p>
+	<?php endif; ?>
+	<?php echo $this->pagination->getPagesLinks(); ?>
+</div>
+<?php endif; ?>
+</div>
 <?php echo AdvPortfolioHelper::poweredBy();
