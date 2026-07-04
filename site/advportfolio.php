28030b72142b013178fdfb7c9d6d3e290e09bddc
Hekla Terriol <webstuff@sternenaugen.net>
2026-07-04 22:03:47 +0200

    Fix site/advportfolio.php: register WebAssets with correct paths

--- site/advportfolio.php
+++ site/advportfolio.php
@@ -15,8 +15,21 @@ use Joomla\CMS\MVC\Controller\BaseController;
 // Include dependencies
 require_once __DIR__ . '/helpers/advportfolio.php';
 require_once __DIR__ . '/helpers/route.php';
+require_once __DIR__ . '/controllers/controller.php';
+
+// Get WebAssetManager
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+
+// REGISTER site assets
+$wa->registerScript('com_advportfolio.script', 'media/js/script.js', [], ['defer' => true]);
+$wa->registerStyle('com_advportfolio.style', 'media/css/style.css');
+
+// USE site assets
+$wa->useScript('com_advportfolio.script');
+$wa->useStyle('com_advportfolio.style');
 
 // Get and execute the controller
+$input = Factory::getApplication()->getInput();
 $controller = BaseController::getInstance('AdvPortfolio', ['default_view' => 'projects']);
-$controller->execute(Factory::getApplication()->getInput()->getCmd('task'));
+$controller->execute($input->getCmd('task'));
 $controller->redirect();
