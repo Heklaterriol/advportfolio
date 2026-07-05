7147b4aad577c7fdf1809672fb9ecb6c609d2210
Hekla Terriol <webstuff@sternenaugen.net>
2026-07-05 18:28:25 +0200

    Create AdvportfolioComponent.php

---
+++ admin/src/Extension/AdvportfolioComponent.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright	Copyright (c) 2013 Skyline Technology Ltd (http://extstore.com). All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace Joomla\Component\Advportfolio\Administrator\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\MVCComponent;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+class AdvportfolioComponent extends MVCComponent
+{
+	public function boot(): void
+	{
+		if (!Factory::getApplication()->getIdentity()->authorise('core.manage', 'com_advportfolio')) {
+			throw new \Exception(Text::_('JERROR_ALERTNOAUTHOR'), 404);
+		}
+
+		HTMLHelper::_('script', 'com_advportfolio/admin.script.js', ['relative' => false, 'version' => true]);
+		HTMLHelper::_('stylesheet', 'com_advportfolio/admin.style.css', ['relative' => false, 'version' => true]);
+
+		HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+	}
+}
